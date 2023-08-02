82935f4f9e43a4e8ddd04f9f9b9d0eeb5d4ae985
sharayura <sharapyura@gmail.com>
2023-08-02 21:14:08 +0300

    Initial commit

---
+++ src/main/java/pro/sky/test_skypro/TestSkyproApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.test_skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSkyproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSkyproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/test_skypro/TestSkyproApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.test_skypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSkyproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
